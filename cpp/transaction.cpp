89eb34de28cb2904a487ad793dab4ce44a8e27e1
thecatblake <k222ryousuke@gmail.com>
2024-05-07 15:09:41 +0900

    add transaction parser

--- cpp/transaction.cpp
+++ cpp/transaction.cpp
@@ -14,14 +14,24 @@ std::string TxIn::toString() {
     return std::string();
 }
 
-TxIn TxIn::parse(unsigned char* serialization, int* len) {
+TxIn TxIn::parse(unsigned char* serialization, long long *len) {
     TxIn txin;
-    size_t i = 0;
+    long long i = 0;
     std::reverse_copy(serialization+i, serialization+32, txin.prev_tx);
     i += 32;
     memcpy((unsigned char*)&txin.prev_index, serialization+i, sizeof(txin.prev_index));
     i += sizeof(txin.prev_index);
     int var_len;
+    auto _script_size = decode_varint(serialization+i, &var_len);
+    long long script_size;
+    memcpy((unsigned char*)&script_size, _script_size.data(), sizeof(script_size));
+    i += var_len;
+    txin.script_raw.resize(script_size);
+    std::copy(serialization+i, serialization+i+script_size, txin.script_raw.begin());
+    i += script_size;
+    memcpy((unsigned char*)&txin.nSequence, serialization+i, sizeof(txin.nSequence));
+    i += sizeof(txin.nSequence);
+    *len = i;
     return txin;
 }
 
@@ -31,6 +41,23 @@ TxOut::TxOut() {
 
 std::string TxOut::toString() {
     return std::string();
+}
+
+TxOut TxOut::parse(unsigned char *serialization, long long *len) {
+    TxOut txout;
+    long long i = 0;
+    memcpy((unsigned char*)&txout.amount, serialization+i, sizeof(txout.amount));
+    i += sizeof(txout.amount);
+    int var_len;
+    auto _script_size = decode_varint(serialization+i, &var_len);
+    long long script_size;
+    memcpy((unsigned char*)&script_size, _script_size.data(), sizeof(script_size));
+    i += var_len;
+    txout.script_raw.resize(script_size);
+    std::copy(serialization+i, serialization+i+script_size, txout.script_raw.begin());
+    i += script_size;
+    *len = i;
+    return txout;
 }
 
 transaction::transaction(): version(1), lockTime(0) {
@@ -62,7 +89,32 @@ transaction transaction::parse(std::vector<unsigned char> serialization) {
     auto num_inp = decode_varint(&serialization[i], &var_len);
     i += var_len;
     long long n;
-    memcpy((unsigned char*)&n, num_inp.data(), sizeof(long long));
+    memcpy((unsigned char*)&n, num_inp.data(), sizeof(n));
     tx.inputs.resize(n);
+    for(auto j=0; j < n;j++) {
+        long long txin_len;
+        TxIn txin = TxIn::parse(&serialization[i], &txin_len);
+        tx.inputs[j] = txin;
+        i += txin_len;
+    }
+    num_inp = decode_varint(&serialization[i], &var_len);
+    i += var_len;
+    memcpy((unsigned char*)&n, num_inp.data(), sizeof(n));
+    tx.outputs.resize(n);
+    for(auto j=0;j < n; j++) {
+        long long txout_len;
+        TxOut txout = TxOut::parse(&serialization[i], &txout_len);
+        tx.outputs[j] = txout;
+        i += txout_len;
+    }
+    memcpy((unsigned char*)&tx.lockTime, &serialization[i], sizeof(tx.lockTime));
     return tx;
+}
+
+uint64_t transaction::GetOutAmount() {
+    uint64_t amount = 0;
+    for(const auto& output: outputs) {
+        amount += output.amount;
+    }
+    return amount;
 }
--- cpp/transaction.h
+++ cpp/transaction.h
@@ -11,15 +11,20 @@ struct TxIn {
     unsigned char prev_tx[32];
     uint32_t prev_index;
     std::vector<unsigned char> script_raw;
+    uint32_t nSequence;
 
     TxIn();
     std::string toString();
-    static TxIn parse(unsigned char* serialization, int* len);
+    static TxIn parse(unsigned char* serialization, long long* len);
 };
 
 struct TxOut {
+    int64_t amount;
+    std::vector<unsigned char> script_raw;
+
     TxOut();
     std::string toString();
+    static TxOut parse(unsigned char* serialization, long long* len);
 };
 
 class transaction {
@@ -35,6 +40,7 @@ public:
 
     std::vector<unsigned char> serialize();
     static transaction parse(std::vector<unsigned char> serialization);
+    uint64_t GetOutAmount();
 };
 
 #endif //BITCOIN_TRANSACTION_H
